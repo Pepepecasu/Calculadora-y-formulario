3e6644168c8111597010301891a5c404d724dbcd
Pepepecasu <levimisaelt@gmail.com>
2023-02-05 16:39:29 -0600

    Update calculos.js

--- Prototipo/calculos.js
+++ Prototipo/calculos.js
@@ -5,7 +5,7 @@ function ops() { //la funcion ops es para hacer las operaciones
     let ai= document.getElementById(a).value; //se relacionan las tres variables con los inputs 
     let Δti= document.getElementById(Δt).value;
 
-    let mtv= document.getElementById(mt); //se relaciona el div con el js
+    let mtv= document.createElement('p'); //se crea un parrafo
     let as= ai*Δti; //se crea una variable donde se haga primero el procedimiento de dos valores
     mtv.innerHTML = parseInt(Lii) * 1 * parseInt(as); //se hacen los dos procedimientos mayores 
     document.body.append(mtv);
