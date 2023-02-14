c40b0249f9588250bf6ad1ae1b49f5f4deaced3c
Pepepecasu <levimisaelt@gmail.com>
2023-02-13 22:51:21 -0600

    correcion del archivo JS

--- Prototipo/calculos.js
+++ Prototipo/calculos.js
@@ -1,16 +1,27 @@
 
 //se crea el archivo donde se van a incluir los calculos 
-function ops() { //la funcion ops es para hacer las operaciones 
+//function ops() { //la funcion ops es para hacer las operaciones 
+    //let Lii= document.getElementById(Li).value;
+    //let ai= document.getElementById(a).value; //se relacionan las tres variables con los inputs 
+    //let Δti= document.getElementById(Δt).value;
+
+    //let mtv= document.createElement('p'); //se crea un parrafo
+    //let as= ai*Δti; //se crea una variable donde se haga primero el procedimiento de dos valores
+    //mtv.innerHTML = parseInt(Lii) * 1 * parseInt(as); //se hacen los dos procedimientos mayores 
+    //document.body.append(mtv);
+//}
+function ops() {
+    let div = document.createElement('div');
+    div.className = "alert";
     let Lii= document.getElementById(Li).value;
-    let ai= document.getElementById(a).value; //se relacionan las tres variables con los inputs 
+    let ai= document.getElementById(a).value;
     let Δti= document.getElementById(Δt).value;
 
-    let mtv= document.createElement('p'); //se crea un parrafo
-    let as= ai*Δti; //se crea una variable donde se haga primero el procedimiento de dos valores
-    mtv.innerHTML = parseInt(Lii) * 1 * parseInt(as); //se hacen los dos procedimientos mayores 
-    document.body.append(mtv);
-}
+    div.innerHTML = parseInt(Lii) * 1 + parseInt(ai) * parseInt(Δt) + "m";
+    document.body.append(div);
 
+    
+}
 
 
 
